f3a0e3f3260ed8a4dfe4003efb9919e22431e9c1
Steve Quince <squince@glgroup.com>
2024-10-22 23:28:24 -0400

    initial commit - basic splash screen works-at-all

---
+++ README.md
@@ -0,0 +1,15 @@
+# pantryPlus
+
+Manage your shopping lists, and then some.
+
+## Run
+This project relies on XCode, node, typescript, react, and expo.
+Ensure you have the correct version of [node.js](https://nodejs.org/) installed.
+We also recommend that you use [nvm](https://nvm.sh) to manage different versions.
+
+Once these are installed, follow these steps to get the project running:
+
+1. `nvm use` - Switch to the correct node.js version
+1. `npm install` to get all dependencies
+1. `npm start` to run the project
+1. Follow the on-screen instructions to open the app on your iOS or Android device's Expo Go app.
---
+++ src/views/SplashScreen/style.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: '#841584',
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
+    alignItems: 'center',
+    margin: 'auto',
+    padding: '10%',
+    justifyContent: 'center'
+  },
+  welcomeText: {
+    color: 'white',
+    fontSize: 30,
+    padding: '0%'
+  },
+  pantryLogo: {
+    color: 'white',
+    fontSize: 60,
+    padding: '0%'
+  },
+  plusLogo: {
+    fontSize: 80,
+    padding: '0%',
+    color: 'white'
+  },
+  logoContainer: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  welcomeContainer: {
+    display: 'flex',
+    flexDirection: 'row'
+  }
+});
