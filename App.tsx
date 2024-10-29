45280788208d68691402f81d2b3512673237421a
Steve Quince <squince@glgroup.com>
2024-10-28 23:10:30 -0400

    very basic auth page displaying

--- App.tsx
+++ App.tsx
@@ -1,9 +1,22 @@
 import { useState, useEffect } from 'react';
+import { Button } from 'react-native';
+import { Amplify } from "aws-amplify";
+import { useAuthenticator, Authenticator } from '@aws-amplify/ui-react-native';
+
 import SplashScreen from 'src/views/SplashScreen/index';
 import IntroScreen from 'src/views/IntroScreen/index';
+import amplifyConfig from 'src/config/amplify';
+
+Amplify.configure(amplifyConfig);
+
+function SignOutButton() {
+  const { signOut } = useAuthenticator();
+  return <Button onPress={signOut} title="Sign Out" />;
+}
 
 export default function App() {
   const [hideSplashScreen, setHideSplashScreen] = useState(false);
+  const [hideIntroScreen, setHideIntroScreen] = useState(false);
 
   useEffect(() => {
     setHideSplashScreen(false);
@@ -15,5 +28,14 @@ export default function App() {
   if (!hideSplashScreen) {
     return <SplashScreen />;
   }
-  return <IntroScreen />;
+  // if (!hideIntroScreen) {
+  //   return <IntroScreen />;
+  // }
+  return (
+    <Authenticator.Provider>
+      <Authenticator>
+        <SignOutButton />
+      </Authenticator>
+    </Authenticator.Provider>
+  );
 }
---
+++ src/config/amplify.ts
@@ -0,0 +1,39 @@
+import { Amplify, ResourcesConfig } from "aws-amplify";
+
+const EXPO_PUBLIC_USER_POOL_ID = process.env.EXPO_PUBLIC_USER_POOL_ID;
+const EXPO_PUBLIC_APP_CLIENT_ID = process.env.EXPO_PUBLIC_APP_CLIENT_ID;
+const EXPO_PUBLIC_REGION = process.env.EXPO_PUBLIC_REGION;
+
+if (!EXPO_PUBLIC_USER_POOL_ID || !EXPO_PUBLIC_APP_CLIENT_ID) {
+  throw new Error("Required environment variables are not set");
+}
+
+const amplifyConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: EXPO_PUBLIC_USER_POOL_ID,
+      userPoolClientId: EXPO_PUBLIC_APP_CLIENT_ID,
+      identityPoolId: "",
+      region: EXPO_PUBLIC_REGION,
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+};
+
+export default amplifyConfig as ResourcesConfig;
