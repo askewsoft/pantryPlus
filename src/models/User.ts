3961b10613fe94b690e0866fe61c6ff7552c9b1b
Steve Quince <squince@glgroup.com>
2024-11-12 16:00:18 -0500

    finally have a basic rootstore working with async fetching of user details from amplify auth

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,28 +1,8 @@
 import { t } from 'mobx-state-tree';
-import { fetchUserAttributes, FetchUserAttributesOutput } from "aws-amplify/auth";
-
-type FetchUserAttributesType = typeof fetchUserAttributes;
 
 export const UserModel = t
     .model("UserModel",{
-        email: t.optional(t.string, ''),
-        sub: t.optional(t.string, ''),
-        nickname: t.optional(t.string, ''),
-    })
-    .actions(self => ({
-        getUser: function* getUser() {
-            function* transformUserAttributes(fetchUserAttributes: FetchUserAttributesType) {
-                self = yield fetchUserAttributes;
-            };
-            if (self.email && self.sub && self.nickname) {
-                yield self;
-            } else {
-                console.log('fetching user attributes');
-                try {
-                  self = yield transformUserAttributes(fetchUserAttributes);
-                } catch (error) {
-                  console.warn('Unable to fetch user attributes:', error);
-                }
-            }
-        }
-    }));
+        email: t.optional(t.frozen(t.string), ''),
+        sub: t.optional(t.frozen(t.string), ''),
+        nickname: t.optional(t.frozen(t.string), ''),
+    });
