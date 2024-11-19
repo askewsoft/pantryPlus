63548c854274a4eb7bf000e17228e7330b771fe2
Steve Quince <squince@glgroup.com>
2024-11-19 12:22:09 -0500

    consume api to store new users

--- src/models/Shopper.ts
+++ src/models/Shopper.ts
@@ -3,12 +3,6 @@ import { t } from 'mobx-state-tree';
 export const ShopperModel = t
     .model("ShopperModel", {
         id: t.identifier,
-        firstName: t.string,
-        lastName: t.string,
+        nickName: t.string,
         email: t.string,
-    })
-    .views(self => ({
-        get fullName() {
-            return `${self.firstName} ${self.lastName}`;
-        }
-    }));
+    });
