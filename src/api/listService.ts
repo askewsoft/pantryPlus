aa27c13f922924a534e60c4fcd3061af0b452213
Steve Quince <squince@glgroup.com>
2024-11-28 20:32:58 -0500

    remove debug logging

--- src/api/listService.ts
+++ src/api/listService.ts
@@ -10,7 +10,6 @@ const listsApi = new ListsApi(configuration);
 
 const createList = async (list: ListType, ownerId: string, xAuthUser: string) => {
     const { id, name }  = list;
-    console.log(`addList: ${JSON.stringify({ id, name, ownerId, xAuthUser})}`);
     try {
         await listsApi.createList({ id, name, ownerId }, xAuthUser);
     } catch (error) {
