41b4caa5e0d7dcd01f147fa69dabdd5b59f2e424
Steve Quince <squince@glgroup.com>
2024-11-28 21:48:24 -0500

    display 'no lists' message with clickable CTA if no lists

--- src/screens/ListsNavigation/MyLists.tsx
+++ src/screens/ListsNavigation/MyLists.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import DraggableFlatList, { ScaleDecorator } from 'react-native-draggable-flatlist';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { toJS } from 'mobx';
 
 import { StackPropsListsMyLists } from '@/types/ListNavTypes';
@@ -10,31 +10,7 @@ import AddListModal from '@/modals/AddListModal';
 import { domainStore, ListType } from '@/models/DomainStore';
 
 import colors from '@/colors';
-
-const MyLists = ({route, navigation}: StackPropsListsMyLists) => {
-  return (
-    <View>
-      <DraggableFlatList
-        contentContainerStyle={styles.draggableFlatListStyle}
-        data={toJS(domainStore.lists)}
-        renderItem={renderListItem(navigation)}
-        keyExtractor={keyExtractor}
-      />
-      <AddListModal />
-    </View>
-  );
-}
-
-const renderListItem = (navigation: any) => {
-  return ({ item, drag }: { item: ListType, drag: () => void }) => {
-    console.log(`item: ${JSON.stringify(item)}`);
-    return (
-      <ScaleDecorator activeScale={1.04}>
-        <ListItem id={item.id} drag={drag} navigation={navigation}/>
-      </ScaleDecorator>
-    );
-  }
-}
+import { uiStore } from '@/models/UIStore';
 
 const keyExtractor = (item: ListType) => item.id;
 
@@ -44,10 +20,63 @@ const onDragEnd = (data: ListType[], from: number, to: number) => {
   domainStore.lists.splice(to, 0, data[from]);
 }
 
+const renderListItem = (navigation: any) => {
+  return ({ item, drag }: { item: ListType, drag: () => void }) => {
+    return (
+      <ScaleDecorator activeScale={1.04}>
+        <ListItem id={item.id} drag={drag} navigation={navigation}/>
+      </ScaleDecorator>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
   draggableFlatListStyle: {
     backgroundColor: colors.detailsBackground,
+  },
+  noListsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  noListsText: {
+    fontSize: 24,
+    fontStyle: 'italic',
+    color: colors.brandColor,
+    marginBottom: 20,
   }
 });
+
+
+const MyLists = ({route, navigation}: StackPropsListsMyLists) => {
+  const onPressAddList = () => {
+    uiStore.setAddListModalVisible(true);
+  };
+
+  if (domainStore.lists.length <= 0 && !uiStore.addListModalVisible) {
+    return (
+      <View style={styles.noListsContainer}>
+        <Text style={styles.noListsText}>No lists yet.</Text>
+        <Button title="Click here to create a list" onPress={onPressAddList} color={colors.brandColor} />
+      </View>
+    );
+  } else {
+    return (
+      /* IFF DraggableFlatList creates problems,
+      * investigate https://github.com/fivecar/react-native-draglist
+      */
+      <View>
+        <DraggableFlatList
+          contentContainerStyle={styles.draggableFlatListStyle}
+          data={toJS(domainStore.lists)}
+          // onDragEnd={onDragEnd}
+          renderItem={renderListItem(navigation)}
+          keyExtractor={keyExtractor}
+        />
+        <AddListModal />
+      </View>
+    );
+  }
+}
 
 export default observer(MyLists);
