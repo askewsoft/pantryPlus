cb74e23468f913fc8b825aa107ca4a4809c273db
Steve Quince <squince@glgroup.com>
2024-12-17 12:22:11 -0500

    implement feature for adding product items to a list without a category

--- src/screens/ListsNavigation/index.tsx
+++ src/screens/ListsNavigation/index.tsx
@@ -8,11 +8,23 @@ import ShoppingList from './ShoppingList';
 import PurchaseHistory from './PurchaseHistory';
 
 import AddListButton from '@/components/Buttons/AddListButton';
+import AddProductButton from '@/components/Buttons/AddProductButton';
 import AddCategoryButton from '@/components/Buttons/AddCategoryButton';
 import { uiStore } from '@/stores/UIStore';
 import stackNavScreenOptions from '@/consts/stackNavOptions';
+import colors from '@/consts/colors';
 
 const { Navigator, Screen } = createStackNavigator<ListsStackParamList>();
+
+
+/* TODO: Find a way to hide the ItemInput element when the user presses the back button
+   * take a look at https://reactnavigation.org/docs/stack-navigator/#transitionstart
+const onBackPress = () => {
+  uiStore.setAddItemToCategoryID('');
+  uiStore.setAddItemToListID('');
+}
+*/
+
 
 const ListsNavigation = () => {
   const onPressAddList = () => {
@@ -23,10 +35,33 @@ const ListsNavigation = () => {
     uiStore.setAddCategoryModalVisible(true);
   };
 
+  const ListHeaderRight = () => {
+    return (
+      <>
+        <AddProductButton foreground={colors.white} background={colors.brandColor} listId={uiStore.selectedShoppingList!} />
+        <AddCategoryButton foreground={colors.white} background={colors.brandColor} onPress={onPressAddCategory} />
+      </>
+    );
+  }
+
   return (
     <Navigator initialRouteName="MyLists" screenOptions={stackNavScreenOptions}>
-      <Screen name="MyLists" component={MyLists} options={{ title: 'My Lists', headerRight: () => <AddListButton dark={true} onPress={onPressAddList} /> }} />
-      <Screen name="ShoppingList" component={ShoppingList} options={{ headerRight: () => <AddCategoryButton dark={true} onPress={onPressAddCategory} /> }} />
+      <Screen name="MyLists"
+        component={MyLists}
+        options={{
+          title: 'My Lists',
+          headerMode: 'float', // ensures consistent header behavior on Android
+          headerRight: () =>
+            <AddListButton dark={true} onPress={onPressAddList} />,
+        }}
+      />
+      <Screen name="ShoppingList"
+        component={ShoppingList}
+        options={{
+          headerRight: () =>
+            <ListHeaderRight />
+        }}
+      />
       <Screen name="PurchaseHistory" component={PurchaseHistory} />
     </Navigator>
   );
