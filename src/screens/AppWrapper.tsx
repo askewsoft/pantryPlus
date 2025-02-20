9b1bef729ffa15a1492ba8b7bb6c6a94765265a2
Steve Quince <squince@glgroup.com>
2025-02-20 12:49:14 -0500

    moving tab options into a separate consts file

--- src/screens/AppWrapper.tsx
+++ src/screens/AppWrapper.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { observer } from 'mobx-react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator, BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
-import { observer } from 'mobx-react';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 import { AppTabParamList } from '@/types/AppNavTypes';
 import ListsNavigation from './ListsNavigation';
@@ -13,29 +13,12 @@ import GroupsNavigation from './GroupsNavigation';
 import LocationsNavigation from './LocationsNavigation';
 
 import colors from '@/consts/colors';
-import { domainStore } from '@/stores/DomainStore';
-
-const { Navigator, Screen } = createBottomTabNavigator<AppTabParamList>();
-
-const AppWrapper = () => {
-  return (
-    // View is needed to push the status bar to the bottom of the screen
-    // This should not be needed.
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <NavigationContainer>
-        <Navigator initialRouteName="Lists" screenOptions={screenOptions} >
-          <Screen name="Lists" component={ListsNavigation} options={tabOptions({iconName: 'list-alt'})} />
-          <Screen name="Groups" component={GroupsNavigation} options={tabOptions({iconName: 'groups'})} />
-          <Screen name="Locations" component={LocationsNavigation} options={tabOptions({iconName: 'store'})} />
-          <Screen name="Settings" component={SettingsNavigation} options={tabOptions({iconName: 'settings'})} />
-        </Navigator>
-      </NavigationContainer>
-    </View>
-  );
-}
+import tabOptions from '@/consts/tabNavOptions';
 
 type MaterialIconName = React.ComponentProps<typeof MaterialIcons>['name'];
+
+const { Navigator, Screen } = createBottomTabNavigator<AppTabParamList>();
+const screenOptions: BottomTabNavigationOptions = { headerShown: false, lazy: false };
 
 const tabBarIconCreator = (name: MaterialIconName) => {
     return function ({ color, size }: { color: string, size: number }) {
@@ -43,28 +26,23 @@ const tabBarIconCreator = (name: MaterialIconName) => {
     }
 };
 
-// Shared tab options
-const tabOptions = ({iconName}: {iconName: MaterialIconName}): BottomTabNavigationOptions => {
-  const numInvites = domainStore.user?.numInvites || 0;
-  return {
-    tabBarIcon: tabBarIconCreator(iconName),
-    tabBarShowLabel: true,
-    tabBarLabelPosition: 'below-icon',
-    tabBarLabelStyle: { fontWeight: 'bold', fontSize: 14 },
-    tabBarActiveTintColor: colors.white,
-    tabBarInactiveTintColor: colors.inactiveButtonColor,
-    tabBarInactiveBackgroundColor: colors.brandColor,
-    tabBarStyle: { backgroundColor: colors.brandColor, height: 90, paddingBottom: 20, paddingTop: 10 },
-    tabBarItemStyle: { borderRadius: 5, backgroundColor: colors.lightBrandColor },
-    tabBarPosition: 'bottom',
-    tabBarBadge: iconName === 'groups' && numInvites > 0 ? numInvites : undefined,
-    tabBarBadgeStyle: { color: colors.white, backgroundColor: colors.alertColor },
-  }
-};
-
-const screenOptions: BottomTabNavigationOptions = {
-  headerShown: false,
-  lazy: false,
+const AppWrapper = () => {
+  // TODO: set `initialRouteName` based on the uiStore.lastScreen value
+  return (
+    // View is needed to push the status bar to the bottom of the screen
+    // This should not be needed.
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Navigator initialRouteName="Lists" screenOptions={screenOptions} >
+          <Screen name="Lists" component={ListsNavigation} options={{...tabOptions({iconName: 'list-alt'}), tabBarIcon: tabBarIconCreator('list-alt')}} />
+          <Screen name="Groups" component={GroupsNavigation} options={{...tabOptions({iconName: 'groups'}), tabBarIcon: tabBarIconCreator('groups')}} />
+          <Screen name="Locations" component={LocationsNavigation} options={{...tabOptions({iconName: 'store'}), tabBarIcon: tabBarIconCreator('store')}} />
+          <Screen name="Settings" component={SettingsNavigation} options={{...tabOptions({iconName: 'settings'}), tabBarIcon: tabBarIconCreator('settings')}} />
+        </Navigator>
+      </NavigationContainer>
+    </View>
+  );
 }
 
 /* TODO:
