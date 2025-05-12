c6b9b27d5061b168ab644158c93c6901f75cc8c3
Steve Quince <squince@glgroup.com>
2025-05-11 22:02:04 -0400

    playing with colors

--- src/consts/colors.ts
+++ src/consts/colors.ts
@@ -1,9 +1,9 @@
 export default {
   brandColor: '#17305C',
-  lightBrandColor: '#204381',
+  lightBrandColor: '#204381', //'#BDD14B',
   white: '#ffffff',
   detailsBackground: '#f0f0f0',
-  itemBackground: '#e7e7e7',
+  itemBackground: '#e7e7e7', // '#E77F39',
   inactiveButtonColor: '#999999',
   disabledButtonColor: '#999999',
   alertColor: '#5886ae',
