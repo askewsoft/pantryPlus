d72ddeb3c29c4b5c14f8909708253f9537451e4f
Steve Quince <squince@glgroup.com>
2026-08-19 18:08:21 -0400

    reference v3 routes

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,5 @@
 const appConfig = {
-    apiUrl: `${process.env.EXPO_PUBLIC_API_URL}/v2`,
+    apiUrl: `${process.env.EXPO_PUBLIC_API_URL}/v3`,
     debug: process.env.EXPO_PUBLIC_DEBUG === 'true'
 } as const;
 
